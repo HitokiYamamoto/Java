d3466eec3fef0aee07dd6901a86bc01f2cd43282
yamamoto hitoki <brumaire1013@gmail.com>
2023-11-09 00:32:43 +0900

    exercise6_1

---
+++ exercise6_1/Main.java
@@ -0,0 +1,13 @@
+package exercise6_1;
+
+import exercise6_1.comment.Zenhan;
+import exercise6_1.comment.Kouhan;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    Zenhan.doWarusa();
+    Zenhan.doTogame();
+    Kouhan.callDeae();
+    Kouhan.showMondokoro();
+  }
+}
